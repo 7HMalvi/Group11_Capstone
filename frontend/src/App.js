f0db58e04036f115e8222732d112f975061fef24
Hirwa Malvi <hmalvi0450@conestoga.on.ca>
2024-06-06 15:18:53 -0400

    Add App.js file in src folder

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Layout from "./components/layout";
+import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
+import Home from './components/screens/Home';
+import Dashboard from './components/screens/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+                 <Route exact path="/" element={<Login />} />
+                 <Route exact path="/login" element={<Login />} />
+                 <Route exact path="/signup" element={<SignUp />} />
+                 <Route exact path="/home" element={<Home />} />
+                 <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
