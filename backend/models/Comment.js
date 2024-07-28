0189ff4af93f130465e737cd1347894fab3d7500
Niraj Devani <nirajdevani091@gmail.com>
2024-07-28 15:00:01 -0400

    Modify comments.js file.

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,47 +1,27 @@
-const { Router } = require("express");
-const { auth } = require("../middleware/auth");
-const { upload } = require("../utils/multer");
-const {
-    addSong, updateSong, deleteSong, getSong, 
-    getSongFile, getCoverImage, getSongList, 
-    addLike, removeLike,
-    addComment, getComments,
-    addPlaylist, removePlaylist, createPlaylist, deletePlaylist, getPlaylists, updatePlaylist
-} = require("../controllers/songController");
+const mongoose = require('mongoose');
 
 
-
-const songRouter = Router();
-
-
-/* Song */
-songRouter.post("/addsong", auth, upload.fields([
-    { name: 'song', maxCount: 1 },
-    { name: 'coverImage', maxCount: 1 }
-]), addSong);
-songRouter.post("/updatesong/:id", auth, upload.fields([
-    { name: 'song', maxCount: 1 },
-    { name: 'coverImage', maxCount: 1 }
-]), updateSong);
-songRouter.get("/song/:id", auth, getSong);
-songRouter.delete("/deletesong/:id", auth, deleteSong);
-songRouter.get("/getsongfile/:id", auth, getSongFile);
-songRouter.get("/getcoverimage/:id", auth, getCoverImage);
-songRouter.post("/getsonglist", auth, getSongList);
-
-/* Features */
-songRouter.post("/addlike", auth, addLike);
-songRouter.post("/removelike", auth, removeLike);
-
-songRouter.post("/addcomment", auth, addComment);
-songRouter.get("/getcomments/:id", auth, getComments);
-
-songRouter.post("/createplaylist", auth, createPlaylist);
-songRouter.delete("/deleteplaylist/:id", auth, deletePlaylist);
-songRouter.post("/addplaylist", auth, addPlaylist);
-songRouter.post("/removeplaylist", auth, removePlaylist);
-songRouter.post("/getplaylists", auth, getPlaylists);
-songRouter.post("/updateplaylist", auth, updatePlaylist);
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref:'User'
+    },
+    song: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref:'Song'
+    },
+    message: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: { 
+        createdAt: true, 
+        updatedAt: true
+    }
+})
 
 
-module.exports = songRouter;
+const Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment;
