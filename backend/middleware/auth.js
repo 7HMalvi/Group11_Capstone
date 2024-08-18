ae0b7c569d0ce72e6687293fdabcc39e4b646b57
Niraj Devani <nirajdevani091@gmail.com>
2024-08-18 18:37:45 -0400

    Modify auth.js file

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -18,35 +18,8 @@ exports.auth = async (req, res, next) => {
         
         next()
         
-    }
-    catch(e){
+    }catch(e){
         res.status(401).send({error: "Please authenticate...!!!"})
-    }
-}
-
-exports.adminAuth = async (req, res, next) => {
-    try{
-
-        const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, "grooveix")
-        let user = await User.findOne({_id:decoded._id, token})
-         
-        if(!user){
-            throw new Error("Please authenticate...!!!")
-        }
-
-        if(user.userType!=="admin"){
-            throw new Error("Access Forbidden...!!!")
-        }
-
-        req.token = token
-        req.user = user
-        
-        next()
-        
-    }
-    catch(e){
-        res.status(401).send({error: e.message})
     }
 }
 
